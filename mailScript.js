5f4f90199dd294d24826e68047ede76fbaf19d97
yayimahuman <donha@utschools.ca>
2015-11-26 22:45:07 -0500

    Create mailScript.js

---
+++ mailScript.js
@@ -0,0 +1,36 @@
+function checkDay(){
+	var date = new Date();
+	var d = date.getDay();
+	if (d == 0|| d == 5||d == 6){
+		document.getElementById('greetDay').innerHTML = "weekend";
+	}
+	else {
+		document.getElementById('greetDay').innerHTML = "day";
+	}
+}
+function checkFormal() {
+    var x = document.getElementById("myRange").value;
+    if (x==0){
+        document.getElementById("para").innerHTML = "Friendly";
+        document.getElementById("tone1").style.display = "none";
+        document.getElementById("tone2").style.display = "none";
+        $("#tone0").fadeIn();
+    }
+    else if (x==1){
+        document.getElementById("para").innerHTML = "Semi-Formal";
+        document.getElementById("tone0").style.display = "none";
+        document.getElementById("tone2").style.display = "none";
+        $("#tone1").fadeIn();
+    }
+    else if (x==2){
+        document.getElementById("para").innerHTML = "Formal";
+        document.getElementById("tone0").style.display = "none";
+        document.getElementById("tone1").style.display = "none";
+        $("#tone2").fadeIn();
+    }
+}
+function getEmail(x) {
+    if (x==0){document.getElementById("tone0").style.display = "block";}
+    else if (x==1){document.getElementById("tone1").style.display = "block";}
+    else if (x==2){document.getElementById("tone2").style.display = "block";}
+}
