d30ecca933c8202ef319aa5ad23a223d36f21f9a
yayimahuman <donha@utschools.ca>
2015-11-27 22:44:25 -0500

    Update written_email.php

--- written_email.php
+++ written_email.php
@@ -1,26 +1,24 @@
 <!DOCTYPE html>
 <head>
+	<title>Your Email</title>
+	<meta charset="utf-8">
+	<meta name="description" content="Write an email in seconds with IntelliMail. Fast, free to use, ad-free.">
+	<meta name="keywords" content="compare,hosts,webhosts,host,web, webhost,hosting,webhosting,find,best">
+	<meta name="author" content="Harry Dong">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 
-<title>Your Email - Write an Email Using IntelliMail</title>
-<meta charset="utf-8">
-<meta name="description" content="Use our email writing tool to write a concise and eloquently composed email. Fast, free to use, ad-free.">
-<meta name="keywords" content="compare,hosts,webhosts,host,web, webhost,hosting,webhosting,find,best">
-<meta name="author" content="Harry Dong">
-<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+	<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 
+	<script src="./javascript/mailScript.js" type="text/javascript"></script>
+	<link rel="stylesheet" type="text/css" href="./css/mailStyle.css">
 
-<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
-
-<script src="./javascript/mailScript.js" type="text/javascript"></script>
-<link rel="stylesheet" type="text/css" href="./css/mailStyle.css">
-
-<style>
-.bg{background: url('http://www.aamrofreight.net/wp-content/uploads/2014/06/blue-abstract-background.jpg') no-repeat center center;}
-</style>
-
-
+	<style>
+	.bg{
+		background: url('http://www.aamrofreight.net/wp-content/uploads/2014/06/blue-abstract-background.jpg') no-repeat center center;
+	}
+	</style>
 </head>
 
 <body onload="getEmail(<?php echo $_GET["tone"]; ?>);checkDay();">
@@ -33,13 +31,10 @@
 		<?php echo $_GET["recipient"];?>,
 		<br>
 		<br>
-		<p>I hope you're having a good weekend! I just wanted to let you know that I will be away on 
-		<?php echo $_GET["day"]; ?>
-		since I 
-		<?php echo $_GET["reason"];?>. 
-		I will 
-		<?php echo $_GET["actions"]; ?>
-		ask a friend for the homework and catch up on any work I miss. If you would like me to drop by sometime to pick up any handouts I'll need, please let me know.
+		<p>I hope you're having a good <span id="day1"></span>! I just wanted to let you know that I will be away on 
+		<?php echo $_GET["day"]; ?>since I 
+		<?php echo $_GET["reason"];?>. I will 
+		<?php echo $_GET["actions"]; ?>ask a friend for the homework and catch up on any work I miss. If you would like me to drop by sometime to pick up any handouts I'll need, please let me know.
 		<br>
 		<br>
 		Thanks,
@@ -52,13 +47,10 @@
      	<?php echo $_GET["recipient"];?>,
 		<br>
 		<br>
-		<p>I hope your weekend is going well! I just wanted to let you know that I will not be in class on 
-		<?php echo $_GET["day"]; ?>
-		since I 
-		<?php echo $_GET["reason"];?>. 
-		I will
-		<?php echo $_GET["actions"]; ?> 
-		ask a friend for the homework and catch up on any work I miss. If you would like me to drop by sometime to pick up any handouts, please let me know.
+		<p>I hope you are having a nice <span id="day2"></span>! I just wanted to let you know that I will not be in class on 
+		<?php echo $_GET["day"]; ?>since I 
+		<?php echo $_GET["reason"];?>. I will
+		<?php echo $_GET["actions"]; ?> ask a friend for the homework and catch up on any work I miss. If you would like me to drop by sometime to pick up any handouts, please let me know.
 		<br>
 		<br>
 		Thanks,
@@ -71,23 +63,19 @@
       	<?php echo $_GET["recipient"];?>,
 		<br>
 		<br>
-		<p>I hope you are enjoying your weekend! I just wanted to inform you that, unfortunately, I will be unable to attend class on 
-		<?php echo $_GET["day"]; ?> 
-		because of
-		<?php echo $_GET["reason"];?>. 
-		I will 
-		<?php echo $_GET["actions"]; ?> 
-		ask a classmate for the homework and complete any work I miss. If you would like me to pick up any handouts, please let me know.
+		<p>I hope your <span id="day3"></span> is going well! I just wanted to inform you that, unfortunately, I will be unable to attend class on 
+		<?php echo $_GET["day"]; ?> because of
+		<?php echo $_GET["reason"];?>. I will 
+		<?php echo $_GET["actions"]; ?> ask a classmate for the homework and complete any work I miss. If you would like me to pick up any handouts, please let me know.
 		<br>
 		<br>
 		Thanks,
 		<br>
 		<?php echo $_GET["signature"];?>
 	</div>
-<p style="font-size:75%">Sent with the help of IntelliMail.xyz</p>
+
+	<p style="font-size:75%">Sent with the help of IntelliMail.xyz</p>
 </div>
-
-
 
 
 </body>
