cbdf26e2d58db64c66fffb5e490226b902dc73a6
yayimahuman <donha@utschools.ca>
2015-12-14 10:31:20 -0500

    created

---
+++ about.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<head>
+	<title>Your Email</title>
+	<meta charset="utf-8">
+	<meta name="description" content="Write an email in seconds with IntelliMail. Fast, free to use, ad-free.">
+	<meta name="keywords" content="compare,hosts,webhosts,host,web, webhost,hosting,webhosting,find,best">
+	<meta name="author" content="Harry Dong">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+	<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+
+	<link rel="stylesheet" type="text/css" href="./css/mailStyle.css">
+</head>
+
+<body>
+
+<?php include_once "navbar.html";?>
+
+<div class="container>
+
+</body>
+</html>
+
